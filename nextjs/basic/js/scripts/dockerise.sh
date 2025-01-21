d159065b1a471777ce3d4c1659f057ea18d96f4f
ChaosCantrip <dev@chaoscantrip.com>
2025-01-21 01:21:37 +0000

    Updated scripts

--- nextjs/basic/js/scripts/dockerise.sh
+++ nextjs/basic/js/scripts/dockerise.sh
@@ -4,7 +4,7 @@ echo "Downloading files from GitHub..."
 git clone https://github.com/ChaosCantrip/Launchpad.git temp_repo
 
 echo "Copying Dockerfile..."
-cp temp_repo/Dockerfile .
+cp temp_repo/nextjs/basic/js/Dockerfile .
 echo "Changing output to standalone..."
 cp temp_repo/nextjs/basic/js/next.config.js ./next.config.js
 
