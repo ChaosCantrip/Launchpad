dfe257dd99d09cf0e3e35a832f2e7a45f0d6544f
ChaosCantrip <dev@chaoscantrip.com>
2025-01-21 01:27:36 +0000

    Updated scripts

--- nextjs/basic/js/scripts/action.sh
+++ nextjs/basic/js/scripts/action.sh
@@ -4,7 +4,7 @@ echo "Downloading files from GitHub..."
 git clone https://github.com/ChaosCantrip/Launchpad.git temp_repo
 
 echo "Copying GitHub action..."
-cp -r temp_repo/nextjs/basic/js/github_actions/build-and-push.yml ./.github/workflows/build-and-push.yml
+cp --parents temp_repo/nextjs/basic/js/github_actions/build-and-push.yml ./.github/workflows/build-and-push.yml
 
 echo "Removing temporary repository..."
 rm -rf temp_repo
