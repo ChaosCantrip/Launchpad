78f927ef488f8251c827ba6dbc7d306b94cb2b04
ChaosCantrip <dev@chaoscantrip.com>
2025-01-21 01:24:50 +0000

    Updated scripts

--- nextjs/basic/js/scripts/dockerise.sh
+++ nextjs/basic/js/scripts/dockerise.sh
@@ -6,7 +6,7 @@ git clone https://github.com/ChaosCantrip/Launchpad.git temp_repo
 echo "Copying Dockerfile..."
 cp temp_repo/nextjs/basic/js/Dockerfile .
 echo "Changing output to standalone..."
-cp temp_repo/nextjs/basic/js/next.config.js ./next.config.js
+cp temp_repo/nextjs/basic/js/next.config.mjs ./next.config.mjs
 
 echo "Removing temporary repository..."
 rm -rf temp_repo
