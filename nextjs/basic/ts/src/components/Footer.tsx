0c3f70255ac14f50db7e534fc8ebf6c5ffe14a5f
ChaosCantrip <dev@chaoscantrip.com>
2025-10-05 22:26:23 +0100

    Removed existing basic templates

--- nextjs/basic/ts/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Footer.module.css";
-
-export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <h1>Footer</h1>
-        </footer>
-    );
-}
